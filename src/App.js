2473b50e9bd75b35a2527ea42b23b2598088525b
aghayev_kamran <kamranagh@wizardsdev.com>
2022-02-04 16:16:41 +0400

    add to git

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import './App.css';
 import Details from './components/Details/Details';
-import React, { useReducer, useRef, useState } from 'react';
+import React, { useReducer, useRef, useState, useImperativeHandle, useCallback, useDebugValue, useLayoutEffect} from 'react';
 import ThemeContext, { themes } from './components/Theme/Theme';
 import sun from './images/sun.png';
 import moon from './images/moon.png';
+import useLocalStorage from '../src/customHooks/useLocalStorage';
+import useUpdateLogger from './customHooks/useUpdateLogger';
+import useRandomJoke from './customHooks/useRandomJoke';
+import { useEffect } from 'react/cjs/react.development';
 
 const App = () => {
+  const [name, setName] = useLocalStorage('name', '');//custom hooks
+  const joke = useRandomJoke('random', 'joke');//custom hooks
   const [theme, setTheme] = useState(themes.light);
   const [moreInfo, setMoreInfo] = useState(false);
+  const [toggle, setToggle] = useState(true);
+
   const ref = useRef(null);
+  useUpdateLogger(name);//custom
+
+  ////////////////////////
   
+  const [callbackCount, setCallbackCount] = useState(0);
+  const callbackFunction = useCallback(() => {
+    console.log(callbackCount, "callback called"); 
+  }, [callbackCount]); 
+
+  ///////////////////////
+
+  const names = 'ryan'
+
+  useDebugValue(name);
+  
+  useDebugValue(names, (name) => name.toUpperCase());
+
+//////////////////////
+// useEffect VS useLayoutEffect
+
+useLayoutEffect(() => {
+  document.body.style.backgroundColor = "#000000";
+}, [])
+
+
+////////////////////
+
   const changeTheme = () => {
     theme === themes.dark
     ? setTheme(themes.light)
@@ -22,56 +56,124 @@ const App = () => {
   
   const handleFocus = () => {
     ref.current.placeholder = 'Оставьте свой комментарий';
-    ref.current.style.border = '3px solid black'
+    ref.current.style.border = '3px solid black';
   }
   
   const onInputChange = (e) => {
-    console.log(e.target.value);
+    dispatch({type: 'CURRENT_COMMENT', payload: e.target.value});
   }
   
-  const sendComment = () => {
-    dispatch({type: 'ADD_COMMENT', payload: {currentComment: '', like: true}});
+  const initialState = {
+      like: false,
+      currentComment: '',
+      comments: []
+  };
+  
+  const sendLike = () => {
+    dispatch({type: 'SEND_LIKE', payload: true});
   }
 
-  const sendLike = () => {
-    dispatch({type: 'SEND_LIKE', like: true });
+  const sendComment = () => {
+    dispatch({type: 'SET_COMMENTS', payload: state.currentComment});
   }
-  
+
   const reducer = (state, action) => {
     switch(action.type){
-      case 'ADD_COMMENT': 
-        return {
-          ...state,
-          currentComment: action.payload
-        }
       case 'SEND_LIKE':
         return {
           ...state,
-          like: action
+          like: action.payload
         }
-      default: 
-        return state;
+        case 'CURRENT_COMMENT':
+          return {
+            ...state,
+            currentComment: action.payload
+        }
+        case 'SET_COMMENTS':
+          return {
+            ...state,
+            comments: [...state.comments, action.payload]
+          }
+        default: 
+          return state;
+      }
     }
-  }
-  const [data, dispatch] = useReducer(reducer, {currentComment: '', like: false});
-  // console.log(data.currentComment)
-  
-  // console.log(data);
+    
+    const [state, dispatch] = useReducer(reducer, initialState);
+    // console.log('state', state);
 
-
-  return (
-    <ThemeContext.Provider value={theme}>
+    return (
+      <ThemeContext.Provider value={theme}>
         <button onClick={handleClick}>More info</button>
         <img src={theme === themes.light ? moon : sun} onClick={changeTheme} alt='sun' />
-        <Details handleClick={handleClick} moreInfo={moreInfo} setMoreInfo={setMoreInfo} />
+        <Details handleClick={handleClick} moreInfo={moreInfo} setMoreInfo={setMoreInfo} toggle={toggle} setToggle={setToggle} />
         <input ref={ref} onChange={onInputChange} />
         <button onClick={handleFocus}>Focus</button>
-        <button onClick={sendComment}>Send comment</button>
+        <button onClick={sendComment}>Send Comment</button>
         <button onClick={sendLike}>Like</button>
-        {data.currentComment ? <p>{data.currentComment}</p> : <></>}
-        {data.like ? <p>Thank you for like</p> : <p>Click like</p>}
+        {state.like ? <p>Thank you for like</p> : <></>}
+        {state.comments.map((item) =>
+            <p key={item.index}>{item}</p>
+        )}
+        <>
+          <p className="quotes"> Generate Joke of the day</p>
+          <p>{joke}</p>        
+        </>
+        <>    
+          <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+          <button onClick={() => setCallbackCount(callbackCount + 1)}>+</button>   
+          <button onClick={callbackFunction}>   
+            Change callback count
+          </button>    
+        </> 
     </ThemeContext.Provider>
   );
 }
 
 export default App;
+
+/// useImperativeHandle, useLayoutEffect, useDebugValue hooks 
+
+// useImperativeHandle(ref, createHandle, [deps])
+
+// const [value, setValue] = useState("");
+// const [error, setError] = useState("");
+// const onChange =(e) => {
+//   setError("");
+//   setValue(e.target.value);
+// }
+
+// useImperativeHandle(ref, () => ({
+//   value: value,
+//     onBlur = () => {
+//     ref.current.blur()
+//   },
+//   clear: () => {
+//     setValue("")
+//     setError("")
+//   },
+//   setError: setError
+// }))
+
+// //useLayoutEffect VS useEffect
+
+
+// const Component = () => {
+//   const [value, setValue] = useState(0);
+
+//   useEffect(() => {     //useLayoutEffect
+//     if (value === 0) {
+//       setValue(Math.random() * 99 + 99);
+//     }
+//   }, [value]);
+
+//   console.log("render", value);
+//   return <div onClick={() => setValue(0)}>value: {value}</div>;
+// };
+
+
+
+
+
+
+
--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useLayoutEffect, useMemo, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import ThemeContext from "../Theme/Theme";
 import burger1 from '../../images/burger1.jpg';
 import burger2 from '../../images/burger2.jpg';
@@ -9,15 +9,25 @@ const sum = (n) => {
     return n + n;
 }
 
-const Details = ({moreInfo, setMoreInfo}) => {
-    const [burgers, setBurgers] = useState([]);
+const Details = ({moreInfo, setMoreInfo, toggle, setToggle}) => {
     const theme = useContext(ThemeContext);
-
+    const [burgers, setBurgers] = useState([]);
     const [num, setNum] = useState(0);
-
     const [burger, setBurgerImage] = useState(false);
-
+    
     // const countSum = sum(num);
+
+    const [update, setUpdate] = useState(100);
+    console.log(update);
+
+    const updateCounter = () => {
+        setUpdate(update - 1)
+    }
+    
+    useEffect(() => {
+        console.log('Update');
+    },[update])
+    
     const countSum = useMemo(() => sum(num), [num]);
 
     const api = async () => {
@@ -28,12 +38,19 @@ const Details = ({moreInfo, setMoreInfo}) => {
     }
 
     useEffect(() => {
-        console.log('mount')
+        return () => {
+            console.log('component will unmount');
+            setToggle(false);
+        }
+    },[toggle]);
+    
+    useEffect(() => {
+        console.log('mount, useEffect')
         api();
     },[]);
 
     // useLayoutEffect(() => {
-    //     console.log('mount')
+    //     console.log('mount, useLayoutEffect')
     //     api();
     // });
 
@@ -43,6 +60,19 @@ const Details = ({moreInfo, setMoreInfo}) => {
 
     return (
         <div className='main' style={theme}>
+            <div>
+                <button onClick={() => setToggle(false)}>Close Menu</button>
+                {toggle
+                ? <div>
+                    <p style={{backgroundColor: 'red', width: '2    00px'}}>Название всех бургеров</p>
+                    {burgers.map((item) => 
+                        <p key={item.id}>{item.name}</p>
+                    )}
+                </div>
+                :
+                <></>
+                }
+            </div>
             <div className='burgerImageWrapper'>
                 <img src={burger ? burger1 : burger2} alt='burger' className='burgerImage' onClick={() => setBurgerImage(!burger)} />
                 <div className='burgerCounter'>
@@ -52,14 +82,16 @@ const Details = ({moreInfo, setMoreInfo}) => {
             </div>
             <div className='flexWrapper'>
             {burgers.map((item) => 
-            <div className='wrapper' style={{borderColor: 'theme.borderColor'}}>
-                <img key={item.id} className='detail' src={item.imageUrl} alt=""  />
-                {moreInfo && 
-                        <p key={item.index}>{item.name}</p>
+                <div key={item.id} className='wrapper' style={{borderColor: 'theme.borderColor'}}>
+                    <img key={item.id} className='detail' src={item.imageUrl} alt=""  />
+                    {moreInfo && 
+                        <p key={item.id}>{item.name}</p>
                 }
             </div>
             )}
             </div>
+            <p>componentDidUpdate example: {update}</p>
+            <button onClick={updateCounter}>-</button>
         </div>
     )
 }
